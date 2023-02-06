d7322fa94e973e3701db4eb929e5b922ecf2025d
Aayushh1003 <122436974+Aayushh1003@users.noreply.github.com>
2023-02-06 15:13:32 +0530

    third commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,41 +2,64 @@ import React, { Component } from 'react'
 
 import {
     Link
-  } from "react-router-dom";
+} from "react-router-dom";
 
 export default class Navbar extends Component {
 
+    handleType = ()=>{
+        let search = document.getElementById('searchTxt');
+        search.addEventListener("input",function(){
+            let inputVal = search.value;
+            console.log("type",inputVal);
+            let card = document.getElementsByClassName('card');
+            Array.from(card).forEach(function(element){
+                let cardTxt = element.getElementsByTagName("p")[0].innerText;
+                // console.log(cardTxt);
 
-  render() {
-    return (
-        <div>
-            <nav className="navbar navbar-expand-lg bg-body-tertiary">
-                <div className="container-fluid">
-                    <Link className="navbar-brand" to="/">NewsMonkey</Link>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li className="nav-item">
-                                <Link className="nav-link" aria-current="page" to="/">Home</Link>
-                            </li>
-                            {/* <li className="nav-item">
+                if(cardTxt.includes(inputVal)){
+                    element.style.display = "flex";
+                }
+
+                else{
+                    element.style.display = "none";
+                }
+            })
+        })
+    }
+    render() {
+        return (
+            <div>
+                <nav className="navbar navbar-expand-lg bg-body-tertiary">
+                    <div className="container-fluid">
+                        <Link className="navbar-brand" to="/">NewsMonkey</Link>
+                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                                <li className="nav-item">
+                                    <Link className="nav-link" aria-current="page" to="/">Home</Link>
+                                </li>
+                                {/* <li className="nav-item">
                                 <a Link className="nav-link" to="/about">About</a>
                             </li> */}
 
-                            <li className = "nav-item"> <Link className="nav-link" to="/business">business</Link> </li>
-                            <li className = "nav-item"> <Link className="nav-link" to="/enterntainment">enterntainment</Link></li>
-                            <li className = "nav-item"> <Link className="nav-link" to="/general">general</Link></li>
-                            <li className = "nav-item"> <Link className="nav-link" to="/health">health</Link></li>
-                            <li className = "nav-item"> <Link className="nav-link" to="/science">science</Link></li>
-                            <li className = "nav-item"> <Link className="nav-link" to="/sports">sports</Link></li>
-                            <li className = "nav-item"> <Link className="nav-link" to="/technology">technology</Link></li>
-                        </ul>
+                                <li className="nav-item"> <Link className="nav-link" to="/business">business</Link> </li>
+                                <li className="nav-item"> <Link className="nav-link" to="/enterntainment">enterntainment</Link></li>
+                                <li className="nav-item"> <Link className="nav-link" to="/general">general</Link></li>
+                                <li className="nav-item"> <Link className="nav-link" to="/health">health</Link></li>
+                                <li className="nav-item"> <Link className="nav-link" to="/science">science</Link></li>
+                                <li className="nav-item"> <Link className="nav-link" to="/sports">sports</Link></li>
+                                <li className="nav-item"> <Link className="nav-link" to="/technology">technology</Link></li>
+                            </ul>
+                            <form className="d-flex" role="search" >
+                                <input className="form-control me-2" id="searchTxt" type="search" placeholder="Search" aria-label="Search" onClick={this.handleType}/>
+                                    <button className="btn btn-outline-success" type="submit" id="mySearch" > Search</button>
+                            </form>
+                        </div>
                     </div>
-                </div>
-            </nav>
-        </div>
-    )
-  }
+                </nav>
+            </div>
+        )
+    }
 }
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -3,16 +3,19 @@ import React, { Component } from 'react'
 export default class NewsItem extends Component {
 
     render() {
-        let { title, description, imageUrl, newsUrl,author,date} = this.props;
+        let { title, description, imageUrl, newsUrl, author, date, source } = this.props;
         return (
             <div className="my-3">
                 <div className="card">
+                    <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left:'90%',zIndex:'1'}}>
+                        {source}
+                    </span>
                     <img src={!imageUrl ? "https://images.moneycontrol.com/static-mcnews/2023/01/Collage-Maker-27-Jan-2023-08.25-AM-770x435.jpg" : imageUrl} className="card-img-top" alt="..." />
                     <div className="card-body">
                         <h5 className="card-title">{title}</h5>
                         <p className="card-text">{description}...</p>
                         <div className="card-footer">
-                            <small className="text-muted">By {!author?"Unknown":author} on {date}</small>
+                            <small className="text-muted">By {!author ? "Unknown" : author} on {new Date(date).toGMTString()}</small>
                         </div>
                         <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-dark btn-sm btn-primary">Read More</a>
                     </div>
